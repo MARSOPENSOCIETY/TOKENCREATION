52065eb4218c2461f65dc5c7d543f9ebba49903f
AtreyuGreen <g_atreyu@hotmail.com>
2018-09-09 17:17:53 +0200

    First branch

---
+++ SafeMath.sol
@@ -0,0 +1,34 @@
+///Es la versión del solidity que se va a utilizar en el compilador
+pragma solidity ^0.4.24;
+
+///Se creará una librería que en este caso es SafeMath.
+///
+library SafeMath {
+  ///Se crean estas funciones para asegurar que las funciones se han completado correctamente
+  function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+    if (a == 0) {
+      return 0;
+    }
+    uint256 c = a * b;
+    assert(c / a == b);
+    return c;
+  }
+
+  function div(uint256 a, uint256 b) internal pure returns (uint256) {
+    // assert(b > 0); // Solidity automatically throws when dividing by 0
+    uint256 c = a / b;
+    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+    return c;
+  }
+
+  function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+    assert(b <= a);
+    return a - b;
+  }
+
+  function add(uint256 a, uint256 b) internal pure returns (uint256) {
+    uint256 c = a + b;
+    assert(c >= a);
+    return c;
+  }
+}
